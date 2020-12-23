33d30f863aa8f316b2a8a403683848274002037f
Isabella Dean <Bean6754@users.noreply.github.com>
2020-12-23 10:03:01 +0000

    Update iexplore_policies.bat

--- Reg/iexplore_policies.bat
+++ Reg/iexplore_policies.bat
@@ -1,4 +1,4 @@
 @echo off
 
-REM For old Edge -- reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main" /v "AllowInPrivate" /t REG_DWORD /d 0 /f
+REM For old Edge -- reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main" /v "AllowInPrivate" /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Internet Explorer\Privacy" /v "EnableInPrivateBrowsing" /t REG_DWORD /d 1 /f
