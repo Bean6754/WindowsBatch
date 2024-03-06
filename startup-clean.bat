9ed037874365cba9196ad421b242305f96109df4
Isabella <Bean6754@users.noreply.github.com>
2024-03-06 15:59:39 +0000

    Update startup-clean.bat

--- startup-clean.bat
+++ startup-clean.bat
@@ -2,12 +2,18 @@
 title Startup Cleaning Script
 REM color 0e
 
+REM This script requires Admin-rights to run so it is advised to either run as SYSTEM before login or create a UAC exception.
+
+REM cd into directories to rmdir /s /q says 'file is in use by another process' so it keeps the folder but deletes all files and folders inside of it.
+REM `&&` executes this command only if previous command's errorlevel is 0.
+
 cls
 echo Stage 1: Temporary file location contents.
 REM Temporary file location contents.
-rmdir /s /q %tmp%\.
-rmdir /s /q %windir%\Temp\.
-rmdir /s /q %windir%\Logs\.
+cd %tmp% && rmdir /s /q %tmp%\.
+cd %windir%\Temp && rmdir /s /q %windir%\Temp\.
+cd %windir%\Logs && rmdir /s /q %windir%\Logs\.
+echo Stage 1 complete.
 
 echo.
 echo Stage 2: Windows Update.
@@ -15,8 +21,9 @@ REM Windows Update.
 net stop bits
 net stop cryptsvc
 net stop wuauserv
-rmdir /s /q %windir%\SoftwareDistribution\.
+cd %windir%\SoftwareDistribution && rmdir /s /q %windir%\SoftwareDistribution\.
 net start wuauserv
+echo Stage 2 complete.
 
 REM pause
 exit
